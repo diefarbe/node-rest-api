82a533e614c10e797bb63a83718b98b445daf017
chris13524 <chris@hichris.com>
2018-07-17 17:48:45 -0400

    Added debug logging to travis_dockerpush.sh.

--- travis_dockerpush.sh
+++ travis_dockerpush.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+echo "script started"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo $?
+echo "logged in"
 docker push diefarbe/sich-ausruhen
+echo $?
+echo "done"
